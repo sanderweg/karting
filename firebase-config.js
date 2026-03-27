4e27bee7842cee01dc01d050aabaead86191561a
sanderweg <140747509+sanderweg@users.noreply.github.com>
2026-03-27 19:11:17 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
 window.DASHBOARD_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyC38ED_Kayrzx5zGIzX9vhABsyEaIdCIOw",
+  authDomain: "race-dashboard-live.firebaseapp.com",
+  projectId: "race-dashboard-live",
+  storageBucket: "race-dashboard-live.firebasestorage.app",
+  messagingSenderId: "182217892782",
+  appId: "1:182217892782:web:01dccaa5a256cfe8145e3f"
 };
